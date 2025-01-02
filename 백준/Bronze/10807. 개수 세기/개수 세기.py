bbab1d51a5d9da7b90a0a77587cbfbb14312e58f
torabo <cheonwonbeom@gmail.com>
2025-01-02 10:44:36 +0900

    Update 개수 세기.py

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,3 +1,5 @@
+'''
+sol1)
 n = int(input())
 list = input().split()
 v = input()
@@ -5,4 +7,22 @@ a=0
 for i in range(n):
     if list[i] == v :
        a+=1
-print(a)
+print(a)'''
+
+#sol2
+N = int(input())
+arr = list(map(int,input().split()))
+v = int(input())
+print(arr.count(v))
+
+
+'''sol3
+n = int(input())
+list = input().split()
+v = input()
+a=0
+for i in list :
+    if i == v:
+       a+=1
+print(a)'''
+
