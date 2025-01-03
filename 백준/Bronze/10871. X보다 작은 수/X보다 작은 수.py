f3b9c32be0065e201f5fd3b04a406449be608964
torabo <cheonwonbeom@gmail.com>
2025-01-03 13:18:43 +0900

    Update X보다 작은 수.py

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,26 @@
+n,x = map(int,input().split())
+arry = list(map(int,input().split()))
+
+
+for i in range(n) :
+    if arry[i] < x :
+        print(arry[i] ,end = ' ')
+
+'''sol2
 n , x = map(int, input().split())
 arry = list(map(int, input().split()))
 
 for i in arry:
     if i < x:
         print(i, end = ' ')
+'''
+
+'''sol3
+n,x = map(int,input().split())
+arry = list(map(int,input().split()))
+
+result = [str(i) for i in arry if i <x]
+
+print(" ".join(result))
+
+'''
