8a9f4ad7e07738a3267b33df3b096ab1b8d38679
torabo <cheonwonbeom@gmail.com>
2025-01-14 03:57:18 +0900

    Update 알파벳 찾기.py

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -5,4 +5,6 @@ for i in range(97,123) :
         print(s.index(chr(i)) , end = ' ')
     else :
         print(-1, end = ' ')
-        
+
+
+# index는 없는거 받으면 오류 뜨지만 find쓰면 없는거 -1로 변환함. 아마 find쓰는걸 원하는 문제 였던것 같음.
