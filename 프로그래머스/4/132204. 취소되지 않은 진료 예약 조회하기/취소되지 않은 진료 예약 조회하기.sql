fbffa518e9f8c6947f247c1da747b41ec5b84c49
torabo <cheonwonbeom@gmail.com>
2025-03-04 14:11:13 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,12 @@
+SELECT app.APNT_NO
+        , p.PT_NAME
+        , p.PT_NO
+        , app.MCDP_CD
+        , d.DR_NAME
+        , app.APNT_YMD
+FROM APPOINTMENT app
+    INNER JOIN PATIENT as p ON p.PT_NO = app.PT_NO
+    INNER JOIN DOCTOR as d ON d.DR_ID = app.MDDR_ID
+WHERE APNT_YMD BETWEEN '2022-04-13 00:00:00' AND '2022-04-13 23:59:59'
+AND APNT_CNCL_YN = 'N'
+ORDER BY APNT_YMD ASC
